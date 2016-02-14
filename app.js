89224226d1c28e0ee6edc78eab2b87c4f8fa8b41
think2011 <452125301@qq.com>
2016-02-15 00:21:28 +0800

    feat 区分开发环境并处理

--- app.js
+++ app.js
@@ -6,18 +6,21 @@ import webpackDevMiddleware from 'koa-webpack-dev-middleware';
 import webpackHotMiddleware from 'koa-webpack-hot-middleware';
 import webpackCfg from './webpack.config';
 
+const DEBUG    = process.env.NODE_ENV === 'DEBUG';
+const PORT     = process.env.PORT || 3000;
 const app      = koa();
 const compiler = webpack(webpackCfg);
-const PORT     = process.env.PORT || 3000;
 
-app.use(serve('./app'));
+if (DEBUG) {
+    app.use(webpackDevMiddleware(compiler, {
+        noInfo    : true,
+        publicPath: webpackCfg.output.publicPath
+    }));
 
-app.use(webpackDevMiddleware(compiler, {
-    noInfo    : true,
-    publicPath: webpackCfg.output.publicPath
-}));
+    app.use(webpackHotMiddleware(compiler));
+}
 
-app.use(webpackHotMiddleware(compiler));
+app.use(serve('./dist'));
 
 app.listen(PORT, err => {
     console.log(`Listening on port ${PORT}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 'use strict';
-const path    = require('path');
-const webpack = require('webpack');
+const path              = require('path');
+const webpack           = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    devtool: 'cheap-module-eval-source-map',
+const DEBUG = process.env.NODE_ENV === 'DEBUG';
+
+const paths = {
+    src : './app',
+    dist: './dist'
+};
+
+var webpackCfg = {
     entry  : [
-        'webpack-hot-middleware/client',
-        './app/index'
+        `${paths.src}/index`
     ],
     output : {
-        path    : path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path    : path.join(__dirname, paths.dist),
+        filename: 'bundle.[hash:8].js'
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
     plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new HtmlWebpackPlugin({
+            template: `${paths.src}/index.html`
+        })
     ],
     module : {
         loaders: [
@@ -44,3 +50,27 @@ module.exports = {
         ]
     }
 };
+
+// 定制开发环境
+if (DEBUG) {
+    webpackCfg.devtool = 'cheap-module-eval-source-map';
+    webpackCfg.entry.unshift('webpack-hot-middleware/client');
+    webpackCfg.output.filename = 'bundle.js';
+    webpackCfg.plugins.push(
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    );
+} else {
+    webpackCfg.plugins.push(
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    );
+}
+
+
+module.exports = webpackCfg;
